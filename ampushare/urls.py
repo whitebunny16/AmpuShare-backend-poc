fae3ebe39c7dc3efeadd1c763764d089d0d6e9b9
thebluedragon7 <71325159+Thebluedragon7@users.noreply.github.com>
2024-03-01 02:36:34 +0545

    UPDATED: post media to nullable, login url; ADDED: Migrations for Post Module;

--- ampushare/urls.py
+++ ampushare/urls.py
@@ -13,16 +13,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/booking/', include('booking.urls')),
     path('api/social/', include('social.urls')),
     path('api/user/', include('user.urls')),
-    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from .views import *
 
 urlpatterns = [
     path('user-detail', get_user_detail),
     path('create-user', get_user_detail),
-    path('delete-user', get_user_detail)
+    path('delete-user', get_user_detail),
+    
+    # Auth
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
