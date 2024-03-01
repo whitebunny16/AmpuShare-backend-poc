9bfd1522cb9f4befe2e4c4780d2a9145da7a6cd5
thebluedragon7 <71325159+Thebluedragon7@users.noreply.github.com>
2024-03-01 21:58:58 +0545

    REFACTORED: Profile Model; ADDED: Migration for updated profile model;

--- user/models.py
+++ user/models.py
@@ -7,17 +7,12 @@ class User(AbstractUser):
 
 
 class Profile(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
     profile_pic = models.ImageField(upload_to='media/images/')
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=1)
-    is_active = models.BooleanField(default=False)
-    username = models.CharField(unique=True, max_length=16)
     phone_number = models.CharField(max_length=15, unique=True, blank=True, null=True)
-    password = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
