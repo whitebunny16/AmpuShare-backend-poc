b76d0c805a3f05881def76a93221759eea4881f4
whitebunny16 <155283269+whitebunny16@users.noreply.github.com>
2024-03-04 09:31:01 +0545

    formatted code and removed comment

--- ampushare/urls.py
+++ ampushare/urls.py
@@ -1,21 +1,8 @@
-"""
-URL configuration for ampushare project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import debug_toolbar
 
 admin.site.site_header = 'AmpuShare Admin'
 admin.site.index_title = 'Admin'
