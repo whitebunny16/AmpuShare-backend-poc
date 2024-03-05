cbfcf346ab17ee1388233f82844babb89b5e1401
thebluedragon7 <71325159+Thebluedragon7@users.noreply.github.com>
2024-03-05 08:12:21 +0545

    UPDATED: Token refresh endpoint

--- ampushare/urls.py
+++ ampushare/urls.py
@@ -3,6 +3,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 admin.site.site_header = 'AmpuShare Admin'
 admin.site.index_title = 'Admin'
@@ -13,4 +16,5 @@ urlpatterns = [
                   path('api/social/', include('social.urls')),
                   path('api/user/', include('user.urls')),
                   path('__debug__/', include(debug_toolbar.urls)),
+                  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
